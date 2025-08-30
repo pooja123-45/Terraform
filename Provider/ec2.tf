1a6562b5840d8d029852fc420e0215bf1a5ab8dc
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-08-30 18:44:18 +0530

    Update ec2.tf

--- Provider/ec2.tf
+++ Provider/ec2.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "firstinstance" {
     ami = "ami-0b016c703b95ecbe4"
     instance_type = "t2.micro"
     key_name = "mykey"
-    security_groups = ["sg-046ab9b2c3264329a"]
+    vpc_security_group_ids = ["sg-046ab9b2c3264329a"]
 
 }
