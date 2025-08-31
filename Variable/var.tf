5fecd93505e9ef6546649602980aa3eed97bc9ba
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-08-31 18:55:53 +0530

    Update var.tf

--- Variable/var.tf
+++ Variable/var.tf
@@ -1,14 +1,16 @@
-variable "this.ami" {
+variable "this_ami" {
   default = "ami-0b016c703b95ecbe4"
 }
 
-variable "this.instance_type" {
+variable "this_instance_type" {
   default = "t2.micro"
 }
 
-variable "this.sgid" {
+variable "this_sgid" {
   default = "sg-046ab9b2c3264329a"
 }
+
+#this this way we can do more
 
 
 
