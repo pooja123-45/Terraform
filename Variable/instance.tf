7ae39d7b564c07e1fcc08989d7ecaabf57684e66
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-08-31 18:56:26 +0530

    Update instance.tf

--- Variable/instance.tf
+++ Variable/instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "firstinstance" {
     ami = "var.this_ami"
-    instance_type = "t2.micro"
+    instance_type = "var.this_instance_type"
     key_name = "thisiskey"
     vpc_security_group_ids = ["sg-046ab9b2c3264329a"]
 
