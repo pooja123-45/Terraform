8cf5221821f8b693d8d00f820187059722c00ff9
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-08-31 18:54:05 +0530

    Update var.tf

--- Variable/var.tf
+++ Variable/var.tf
@@ -1,8 +1,14 @@
 variable "this.ami" {
-  default = ""
+  default = "ami-0b016c703b95ecbe4"
 }
 
 variable "this.instance_type" {
   default = "t2.micro"
 }
+
+variable "this.sgid" {
+  default = "t2.micro"
+}
+
+
 
