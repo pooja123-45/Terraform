ecb4984eae9a9cb4440cb2a88e5b73894413c464
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-01 07:47:20 +0530

    Update var.tf

--- Outpurblock/var.tf
+++ Outpurblock/var.tf
@@ -1,1 +1,12 @@
+variable "this_ami" {
+  default = "ami-0b016c703b95ecbe4"
+}
+
+variable "this_instance_type" {
+  default = "t2.micro"
+}
+
+variable "this_sgid" {
+  default = "sg-046ab9b2c3264329a"
+}
 
