04f47f1cbe9d85a63443b248db2999b2d19e53a5
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-01 07:46:36 +0530

    Create publicIp.tf

---
+++ Outpurblock/publicIp.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-2"
+    profile = "mykey"
+}
+
+resource "aws_instance" "firstinstance" {
+    ami = var.this_ami
+    instance_type = var.this_instance_type
+    key_name = "thisiskey"
+    vpc_security_group_ids = [var.this_sgid]
+     tags = {
+        Name = "firstins"
+      }
+
+
+}
