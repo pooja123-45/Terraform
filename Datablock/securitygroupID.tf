b0779871a0c87c23c25ec983057d263a3e3b108a
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-01 08:20:05 +0530

    Update securitygroupID.tf

--- Datablock/securitygroupID.tf
+++ Datablock/securitygroupID.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+    region = "us-east-2"
+    profile = "mykey"
+}
 
+resource "aws_instance" "firstinstance" {
+    ami = "ami-0b016c703b95ecbe4"
+    instance_type = "t2.micro"
+    key_name = "thisiskey"
+    vpc_security_group_ids = ["sg-046ab9b2c3264329a"]
+
+}
+
+data "aws_security_group" "mysecuritygroup" {    #use to extract exiting infrasture data
+     name = "default" # default is the name of my existing security group
+}
