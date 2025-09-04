6bcf6c5f59b8c9355b80fac4d849ae9af202c191
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-04 14:11:58 +0530

    my

---
+++ ModuleBlock/Resources/VPC/output.tf
@@ -0,0 +1,9 @@
+output "subnet_id" {
+     value = aws_subnet.this_public.id  
+}
+ 
+output "aws_vpc_id" {
+  value = aws_vpc.this_vpc.id
+}
+
+#we will pass this subnet id and vpc id inn ec2 instance in var.tf
