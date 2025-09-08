42ff7311f96e862a38a679454b7367af6859f7b6
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-08 19:35:38 +0530

    Update var.tf

--- tfstatefile/var.tf
+++ tfstatefile/var.tf
@@ -1,11 +1,14 @@
 variable "this_aws_instance_instance_type" {
+   default = "t2.micro"
       
 }
 
 variable "this_aws_instance_count" {    
+    default = 1
     
 }
 
 variable "this_aws_ami" {    
+   default = "ami-0e2369dd6317d0d7e"
 
 }
