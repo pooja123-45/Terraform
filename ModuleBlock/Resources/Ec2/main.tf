e54dce4ecdd9076c8ff6647636346b19c29cd30b
pooja123-45 <85453111+pooja123-45@users.noreply.github.com>
2025-09-08 16:20:23 +0530

    Update main.tf

--- ModuleBlock/Resources/Ec2/main.tf
+++ ModuleBlock/Resources/Ec2/main.tf
@@ -5,10 +5,11 @@ resource "aws_instance" "this_inst" {
   # disable_api_termination = var.This_api_termination 
   key_name                = var.This_key
   #vpc_security_group_ids  = var.This_sg
-  subnet_id = 
+  subnet_id = "var.this_aws_instance_subnet"
   tags = {
       Name = var.This_tag
      }
  
 
 }
+
